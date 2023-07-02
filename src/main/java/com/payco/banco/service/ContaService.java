4dce4d35e9a594eaea6ea1982f42fec849e3482b
Giovana <maiiaraoliveiras1@gmail.com>
2023-07-02 02:37:52 -0300

    Adicionado serviço de saque, deposito e consulta

--- src/main/java/com/payco/banco/service/ContaService.java
+++ src/main/java/com/payco/banco/service/ContaService.java
@@ -1,26 +1,77 @@
 package com.payco.banco.service;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.payco.banco.controller.ContaController;
 import com.payco.banco.model.Conta;
+import com.payco.banco.repository.ContaRepository;
 
 @Service
-public class ContaService{
-	
+public class ContaService {
+
+	@Autowired
+	private ContaRepository contaRepository;
+
 	public void sacar(float valorSaque, Conta conta) {
-		
-		if(conta.getSaldo() > valorSaque) {
+
+		logger.info("ContaService: Iniciando método sacar() - valorSaque = {}, conta = {}", valorSaque, conta);
+
+		if (conta.getSaldo() > valorSaque) {
 			conta.setSaldo(conta.getSaldo() - valorSaque);
 		}
 	}
-	
+
 	public void depositar(float valorDeposito, Conta conta) {
-		
+
+		logger.info("ContaService: Iniciando método depositar() - valorDeposito = {}, conta = {}", valorDeposito,
+				conta);
+
 		conta.setSaldo(conta.getSaldo() + valorDeposito);
 	}
-	
+
 	public float consultarSaldo(Conta conta) {
-		
+
+		logger.info("ContaService: Iniciando método consultarSaldo() - conta = {}", conta);
+
 		return conta.getSaldo();
 	}
+
+	private static final Logger logger = LoggerFactory.getLogger(ContaService.class);
+
+	public ResponseEntity<Conta> servicos(String opcao, float valor, Long id) {
+
+		logger.info("ContaService: Iniciando método servicos() - opcao = {}, valor = {}, id = {}", opcao, valor, id);
+
+		Optional<Conta> resposta = contaRepository.findById(id);
+		if (resposta.isPresent()) {
+			Conta conta = resposta.get();
+
+			switch (opcao) {
+			case "consulta":
+				consultarSaldo(conta);
+				break;
+			case "deposito":
+				depositar(valor, conta);
+				contaRepository.save(conta);
+				break;
+			case "saque":
+				sacar(valor, conta);
+				contaRepository.save(conta);
+				break;
+			default:
+				break;
+			}
+			return ResponseEntity.ok(conta);
+
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
 }
