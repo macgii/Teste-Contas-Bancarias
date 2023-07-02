c5f0b47d5f7b3e6f5641090c031499a206803d6e
Giovana <maiiaraoliveiras1@gmail.com>
2023-07-02 02:35:58 -0300

    Adicionado novo endpoint

--- src/main/java/com/payco/banco/controller/ContaController.java
+++ src/main/java/com/payco/banco/controller/ContaController.java
@@ -15,10 +15,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.payco.banco.model.Conta;
 import com.payco.banco.repository.ContaRepository;
+import com.payco.banco.service.ContaService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -32,49 +34,52 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @Tag(name = "Contas", description = "Operações relacionadas aos contas")
 public class ContaController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(ContaController.class);
-	
+
 	@Autowired
 	private ContaRepository contaRepository;
-	
+
+	@Autowired
+	private ContaService contaService;
+
 	@Operation(summary = "Obter contas", description = "Operação que lista todas as contas bancárias.")
 	@ApiResponse(responseCode = "200", description = "Lista de contas encontrada.")
 	@GetMapping
-	public ResponseEntity <List<Conta>> getAll(){
+	public ResponseEntity<List<Conta>> getAll() {
 		logger.info("ContaController: iniciando método getAll()");
 		return ResponseEntity.ok(contaRepository.findAll());
 	}
-	
+
 	@Operation(summary = "Obter conta por ID", description = "Operação que busca conta bancária por ID.")
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "Conta encontrada com sucesso."),
-			@ApiResponse(responseCode = "404", description = "Conta não encontrada")	
-	})
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Conta encontrada com sucesso."),
+			@ApiResponse(responseCode = "404", description = "Conta não encontrada") })
 	@GetMapping("/{id}")
-	public ResponseEntity<Conta> getById(@Parameter(in = ParameterIn.PATH, description = "ID da conta") @PathVariable Long id){
+	public ResponseEntity<Conta> getById(
+			@Parameter(in = ParameterIn.PATH, description = "ID da conta") @PathVariable Long id) {
 		logger.info("ContaController: Iniciando método getById() - Id = {}", id);
-		return contaRepository.findById(id)	
-				.map(resposta -> ResponseEntity.ok(resposta))
+		return contaRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
 				.orElse(ResponseEntity.notFound().build());
 	}
-	
+
 	@Operation(summary = "Criar conta", description = "Operação que cria uma conta bancária.")
 	@ApiResponse(responseCode = "201", description = "Conta criada com sucesso.")
 	@PostMapping
-	public ResponseEntity<Conta> criarConta(@Parameter(in = ParameterIn.DEFAULT, description = "Dados da conta a ser criada") @RequestBody Conta conta){
+	public ResponseEntity<Conta> criarConta(
+			@Parameter(in = ParameterIn.DEFAULT, description = "Dados da conta a ser criada") @RequestBody Conta conta) {
 		logger.info("ContaController: Iniciando método criarConta() - Conta = {}", conta.toString());
 		return ResponseEntity.status(HttpStatus.CREATED).body(contaRepository.save(conta));
 	}
-	
+
 	@Operation(summary = "Atualizar conta por ID", description = "Operação que atualiza dados de uma conta bancária por ID.")
 	@ApiResponse(responseCode = "201", description = "Conta atualizada com sucesso.")
 	@PutMapping("/{id}")
-	public ResponseEntity<Conta> atualizarConta(@Parameter(in = ParameterIn.DEFAULT, description = "Dados da conta a ser criada") @RequestBody Conta conta){
+	public ResponseEntity<Conta> atualizarConta(
+			@Parameter(in = ParameterIn.DEFAULT, description = "Dados da conta a ser criada") @RequestBody Conta conta) {
 		logger.info("ContaController: Iniciando método atualizarConta() - Conta = {}", conta.toString());
 		return ResponseEntity.status(HttpStatus.CREATED).body(contaRepository.save(conta));
 	}
-	
+
 	@Operation(summary = "Deletar conta por ID", description = "Operação que deleta uma conta bancária por ID.")
 	@ApiResponse(responseCode = "200", description = "Conta deletada com sucesso.")
 	@DeleteMapping("/{id}")
@@ -82,7 +87,14 @@ public class ContaController {
 		logger.info("ContaController: Iniciando método deletar() - Id = {}", id);
 		contaRepository.deleteById(id);
 		logger.info("Conta deletada com sucesso.");
-		
+
 	}
-	
+
+	@GetMapping("operacoes/{id}")
+	public ResponseEntity<Conta> operacao(@RequestParam("opcao") String opcao, @RequestParam("valor") float valor,
+			@PathVariable Long id) {
+
+		logger.info("ContaController: Iniciando método operacao() - Id = {}", id);
+		return contaService.servicos(opcao, valor, id);
+	}
 }
