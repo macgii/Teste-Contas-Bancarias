8ab0909f99b1fbe20115cb734503faadeffb3628
Giovana <maiiaraoliveiras1@gmail.com>
2023-09-04 16:42:43 -0300

    fix(sonar): linhas duplicadas

--- src/main/java/com/payco/banco/model/ContaDTO.java
+++ src/main/java/com/payco/banco/model/ContaDTO.java
@@ -1,35 +1,14 @@
 package com.payco.banco.model;
 
-public class ContaDTO {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class ContaDTO {
+	
 	private Long id;
 	private String titular;
 	private long cpf;
-	private int numeroConta;
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getTitular() {
-		return titular;
-	}
-	public void setTitular(String titular) {
-		this.titular = titular;
-	}
-	public long getCpf() {
-		return cpf;
-	}
-	public void setCpf(long cpf) {
-		this.cpf = cpf;
-	}
-	public int getNumeroConta() {
-		return numeroConta;
-	}
-	public void setNumeroConta(int numeroConta) {
-		this.numeroConta = numeroConta;
-	}
-		
+	private int numeroConta;		
 }
