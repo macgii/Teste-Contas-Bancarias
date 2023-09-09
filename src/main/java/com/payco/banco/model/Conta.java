a6af643712529e884f142a8f2d81ad2e9f0fcd23
Giovana <maiiaraoliveiras1@gmail.com>
2023-09-09 01:31:33 -0300

    fix: anotação lombok

--- src/main/java/com/payco/banco/model/Conta.java
+++ src/main/java/com/payco/banco/model/Conta.java
@@ -13,9 +13,14 @@ import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+@Getter
+@Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
